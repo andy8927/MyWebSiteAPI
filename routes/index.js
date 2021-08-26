72346e3f8dce3fa17e0fc7cbbc3dff3757c2701d
andyhsueh <andyhsueh@91app.com>
2021-08-27 00:45:24 +0800

    add pm2

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    console.log('Hello NodeJs 2021-08-26')
-    res.send('Hello NodeJs 2021-08-26')
+    console.log('Hello NodeJs 2021-08-26 123456')
+    res.send('Hello NodeJs 2021-08-26 123456')
 })
 
 module.exports = router;
